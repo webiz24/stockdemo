2625c721af996dae7d63e3c0ef6522c2fd10839d
webiz24 <p.putthipong8@gmail.com>
2025-07-20 09:17:30 +0700

    update 20-07-2025

--- resources/views/layouts/auth2.blade.php
+++ resources/views/layouts/auth2.blade.php
@@ -40,12 +40,11 @@
                         <img src="{{ asset('img/logo-small.png')}}" alt="lock" class="tw-rounded-full tw-object-fill" />
                     </div> --}}
 
-                    <div class="tw-absolute tw-top-2 md:tw-top-5 tw-left-4 md:tw-left-8 tw-flex tw-items-center tw-gap-4"
-                        style="text-align: left">
+                    <div>
                         <a href="{{ url('/') }}">
                             <div
-                                class="lg:tw-w-16 md:tw-h-16 tw-w-12 tw-h-12 tw-flex tw-items-center tw-justify-center tw-mx-auto tw-overflow-hidden tw-p-0.5 tw-mb-4">
-                                <img src="{{ asset('img/logo-small.png')}}" alt="lock" class="tw-object-fill" />
+                                class="tw-flex tw-items-center tw-justify-center tw-mx-auto tw-overflow-hidden tw-p-0.5 tw-mb-4">
+                                <img src="{{ asset('img/Logo2024-01.png')}}" alt="lock" class="tw-object-fill" style="width: 250px; height: auto;" />
                             </div>
                         </a>
                         @if(config('constants.SHOW_REPAIR_STATUS_LOGIN_SCREEN') && Route::has('repair-status'))
@@ -63,34 +62,7 @@
                         @endif
                     </div>
 
-                    <div class="tw-absolute tw-top-5 md:tw-top-8 tw-right-5 md:tw-right-10 tw-flex tw-items-center tw-gap-4"
-                        style="text-align: left">
-                        @if (!($request->segment(1) == 'business' && $request->segment(2) == 'register'))
-                            <!-- Register Url -->
-                            @if (config('constants.allow_registration'))
-                            {{-- <span
-                                class="tw-text-white tw-font-medium tw-text-sm md:tw-text-base">{{ __('business.not_yet_registered') }}
-                            </span> --}}
-
-                            <div class="tw-border-2 tw-border-white tw-rounded-full tw-h-10 md:tw-h-12 tw-w-24 tw-flex tw-items-center tw-justify-center">
-                             <a href="{{ route('business.getRegister')}}@if(!empty(request()->lang)){{'?lang='.request()->lang}}@endif"
-                                    class="tw-text-white tw-font-medium tw-text-sm md:tw-text-base hover:tw-text-white">
-                                    {{ __('business.register') }}</a>
-                            </div>
-
-                                <!-- pricing url -->
-                                @if (Route::has('pricing') && config('app.env') != 'demo' && $request->segment(1) != 'pricing')
-                                    &nbsp; <a class="tw-text-white tw-font-medium tw-text-sm md:tw-text-base hover:tw-text-white"
-                                        href="{{ action([\Modules\Superadmin\Http\Controllers\PricingController::class, 'index']) }}">@lang('superadmin::lang.pricing')</a>
-                                @endif
-                            @endif
-                        @endif
-                        @if ($request->segment(1) != 'login')
-                            <a class="tw-text-white tw-font-medium tw-text-sm md:tw-text-base hover:tw-text-white"
-                                href="{{ action([\App\Http\Controllers\Auth\LoginController::class, 'login'])}}@if(!empty(request()->lang)){{'?lang='.request()->lang}}@endif">{{ __('business.sign_in') }}</a>
-                        @endif
-                        @include('layouts.partials.language_btn')
-                    </div>
+                    
                     <div class="col-md-10 col-xs-8" style="text-align: right;">
 
                     </div>
